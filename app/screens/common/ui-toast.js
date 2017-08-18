6953176f0368cd506c13398c639bc655ea8fac25
yiyinsong <390396674@qq.com>
2017-08-18 17:44:20 +0800

    update toast style

--- app/screens/common/ui-toast.js
+++ app/screens/common/ui-toast.js
@@ -4,6 +4,7 @@ export default (text) => {
   Toast.show(text, {
       duration: 1000,
       backgroundColor: 'rgba(0,0,0,.8)',
-      position: Toast.positions.BOTTOM
+      position: -40,
+      shadow: false
   });
 }
